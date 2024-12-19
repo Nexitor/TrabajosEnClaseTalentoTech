72651a4b8b19fb76fb801a5b6884686715a42d84
Sebastian <mevise@hotmail.es>
2024-12-18 20:27:29 -0500

    Blank line at the end of the file

--- ejNode/index.js
+++ ejNode/index.js
@@ -4,37 +4,34 @@ const http = require('http')
 const hostname = "localhost"
 const port = 3000
 
-const server = http.createServer( (req,res) => {
+const server = http.createServer((req, res) => {
 
-    console.log( req )
+    console.log(req)
 
-    if(req.method == "GET"){
-        
+    if (req.method == "GET") {
+
         let file = ""
 
-        if(req.url == '/'){
+        if (req.url == '/') {
             file = "pagl.html"
-        }else{
+        } else {
             file = req.url
         }
-        
+
         console.log(`file: ${file}`)
 
         res.statusCode = 200
-        res.setHeader( "Content-Type", "text/html" )
-        res.end( "<html><head><title> M P V </title></head><body><h1>  M P V </h1></body></html>" )
-    }else{
+        res.setHeader("Content-Type", "text/html")
+        res.end("<html><head><title> M P V </title></head><body><h1>  M P V </h1></body></html>")
+    } else {
         res.statusCode = 400
-        res.setHeader( "Content-Type", "text/html" )
-        res.end( "<html><head><title> M P V </title></head><body><h1> METODO NO PERMITIDO </h1></body></html>" )
+        res.setHeader("Content-Type", "text/html")
+        res.end("<html><head><title> M P V </title></head><body><h1> METODO NO PERMITIDO </h1></body></html>")
     }
-} )
+})
 
-server.listen( port,hostname, () => {
+server.listen(port, hostname, () => {
 
-    console.log( `Server running at http://${hostname}:${port}/` )
-    
-} )
+    console.log(`Server running at http://${hostname}:${port}/`)
 
-
-
+})
