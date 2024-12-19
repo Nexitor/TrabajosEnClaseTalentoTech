bf99b7750b27fdafb6549f8dd1fabf3aeca2e451
Sebastian <mevise@hotmail.es>
2024-12-18 21:37:16 -0500

    New folder and index update

--- ejNode/index.js
+++ ejNode/index.js
@@ -1,12 +1,12 @@
 
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const hostname = "localhost"
 const port = 3000
 
 const server = http.createServer((req, res) => {
-
-    console.log(req)
 
     if (req.method == "GET") {
 
@@ -18,13 +18,33 @@ const server = http.createServer((req, res) => {
             file = req.url
         }
 
-        console.log(`file: ${file}`)
+        var pathFile = path.resolve(`./public/${file}`)
+        var extFile = path.extname(pathFile)
 
-        res.statusCode = 200
-        res.setHeader("Content-Type", "text/html")
-        res.end("<html><head><title> M P V </title></head><body><h1>  M P V </h1></body></html>")
+        console.log(`file: ${pathFile}`)
+        console.log(`ext: ${extFile}`)
+
+
+        if (extFile == "html") {
+            fs.exists(pathFile, (e) => {
+                if (e) {
+                    res.statusCode = 200
+                    res.setHeader("Content-Type", "text/html")
+                    fs.createReadStream(pathFile).pipe(res)
+                } else {
+                    res.statusCode = 404
+                    res.setHeader("Content-Type", "text/html")
+                    res.end("<html><head><title> M P V </title></head><body><h1>  M P V </h1></body></html>")
+                }
+            })
+        }else {
+            res.statusCode = 405 ;
+            res.setHeader( "Content-Type", "text/html" ) ;
+            res.end( "<html><head><title> M P V </title></head><body><h1>  METODO NO PERMITIDO </h1></body></html>" ) ;
+        }
+
     } else {
-        res.statusCode = 400
+        res.statusCode = 405
         res.setHeader("Content-Type", "text/html")
         res.end("<html><head><title> M P V </title></head><body><h1> METODO NO PERMITIDO </h1></body></html>")
     }
